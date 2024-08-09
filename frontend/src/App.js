22a295247b9e9eab25e0547fa19bc1dad171a988
nehap910 <pokharkarneha2017@gmail.com>
2024-08-09 19:08:28 +0530

    frontend setup done

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Lets build chat App</h1>
+    <button className="btn btn-secondary">Secondary</button>
+    </div>
+  );
+}
+
+export default App;
